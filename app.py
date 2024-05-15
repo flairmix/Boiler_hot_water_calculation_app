f0faa2b4d4923d2084c9462f733c56845bc3cb12
flairmix <d9067246810@gmail.com>
2024-05-16 00:42:23 +0300

    better plot class

--- app.py
+++ app.py
@@ -26,21 +26,66 @@ from widgets.input_fields import InputFields
 from widgets.action_button import ActionButton
 
 
-class MplCanvas(FigureCanvas):
+# class MplCanvas(FigureCanvas):
 
-    def __init__(self, parent=None, width=10, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
+#     def __init__(self, parent=None, width=10, height=4, dpi=100):
+#         fig = Figure(figsize=(width, height), dpi=dpi)
         
-        self.x_data = consumption_1
+#         self.x_data = consumption_1
+#         self.axes = fig.add_subplot(111)
+#         self.axes.plot(self.x_data)
+
+#         FigureCanvas.__init__(self, fig)
+#         self.setParent(parent)
+#         FigureCanvas.setSizePolicy(self,
+#                                    QtWidgets.QSizePolicy.Expanding,
+#                                    QtWidgets.QSizePolicy.Expanding)
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=24, height=8, dpi=100, hours=24, consumption='decrease'):
+        '''
+        consumption = 'decrease' / 'increase'
+        '''
+        # fig = Figure(figsize=(width, height), dpi=dpi)
+
+        fig = plt.figure(figsize=(width, height), dpi=dpi)
+        # self.x_data = consumption_1
+        
+        hours_x = [i for i in range(0, hours)]
+
         self.axes = fig.add_subplot(111)
-        self.axes.plot(self.x_data)
+
+        for i in range(0, len(hours_x), 1):
+            if (i == 0):
+                plt.text(hours_x[i], boiler_1.hw_reserve_and_boil[i]+0.2, f"{round(boiler_1.hw_reserve_and_boil[i], 1)} м3/ч")
+            elif (i != 0 and abs(boiler_1.hw_reserve_and_boil[i] - boiler_1.hw_reserve_and_boil[i-1]) > 0.5):
+                plt.text(hours_x[i]+0.5, boiler_1.hw_reserve_and_boil[i]+0.2, f"{round(boiler_1.hw_reserve_and_boil[i], 1)} м3/ч")
+        
+        for i in range(0, len(hours_x), 1):
+            if (i == 0):
+                plt.text(hours_x[i], boiler_1.consumption_by_hours_24[i]-0.8, f"{round(boiler_1.consumption_by_hours_24[i], 1)} м3/ч")
+            elif (i != 0 and abs(boiler_1.consumption_by_hours_24[i] - boiler_1.consumption_by_hours_24[i-1]) > 0.5):
+                plt.text(hours_x[i], boiler_1.consumption_by_hours_24[i]-0.8, f"{round(boiler_1.consumption_by_hours_24[i], 1)} м3/ч")
+
+
+        self.axes.plot(hours_x, [0] * len(hours_x), "r-")
+        self.axes.plot(hours_x, boiler_1.consumption_by_hours_24, "b.-", label=f'Расход горячей воды из бойлера {boiler_1.t3_boiler} гр')
+        self.axes.plot(hours_x, boiler_1.hw_reserve_and_boil, "g.-", label=f'Запас воды в бойлере {boiler_1.t3_boiler} гр')
+        self.axes.plot(hours_x, boiler_1.boiler_heating_G_list, ".-", label=f'Нагрев воды бойлере до {boiler_1.t3_boiler} гр')
+        
+        plt.title("Запас горячей воды в бойлере")
+        plt.xlabel('hours')
+        plt.ylabel('consumption')
+        plt.grid(True)
+        plt.legend()
 
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
         FigureCanvas.setSizePolicy(self,
                                    QtWidgets.QSizePolicy.Expanding,
                                    QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+        
 
 
 class App(QMainWindow):
@@ -48,6 +93,7 @@ class App(QMainWindow):
         super(App, self).__init__()
         self.setWindowTitle("Расчет бака-аккумулятора по часам")
         self.central_widget = QWidget()
+        # self.central_widget.setMinimumWidth(500)
         self.setCentralWidget(self.central_widget)
         
         # Создаем виджеты для графика и полей ввода
@@ -56,7 +102,7 @@ class App(QMainWindow):
         self.input_fields = InputFields(self.central_widget)
         self.input_fields.setFixedWidth(500)
         self.action_button = ActionButton(self.central_widget)
-        self.mpl = MplCanvas(self.central_widget, width=5, height=4, dpi=100)
+        self.mpl = MplCanvas(self.central_widget, width=24, height=12, dpi=60, hours=boiler_1.days*24)
 
         # Создаем вертикальную сетку для размещения виджетов
         vbox = QVBoxLayout()
